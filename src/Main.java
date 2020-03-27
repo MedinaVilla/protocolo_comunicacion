c9feb5c451cee14e7a74c46aa3ba82e385f00330
Daniel Montes Guerrero <43693529+DanielMontesGuerrero@users.noreply.github.com>
2020-03-27 13:47:26 -0600

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author danim
+ */
+public class Main {
+    public static void main(String[] args){
+	Envia e = new Envia();
+	e.run();
+    }
+}
